df889836a8e848fd80b686f8a1c2e89266b8f4cc
DrMip <ofri.naftali@gmail.com>
2023-11-01 16:06:13 +0200

    Animations
    Added animations, Need to add more transitions. Organized code

--- Lost Signal/Assets/Code/PlayerCode/PlayerMovement.cs
+++ Lost Signal/Assets/Code/PlayerCode/PlayerMovement.cs
@@ -1,68 +1,45 @@
 using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Dynamic;
 using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private int movementSpeed;
-    [SerializeField] private int jumpStrength;
-    
+    //components of the player
+    private Rigidbody2D rb;
+    private Animator ani;
+    //other components
     [SerializeField] private Transform groundSensor;
-
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private float delayJump  = 0.5f;
-    private float delayCounter;
+    //variables for checking surroundings
     bool isGrounded;
     bool wasGrounded;
     bool isStuckOnLeftWall;
     bool isStuckOnRightWall;
-    bool isOnStairLeft;
-    bool isOnStairRight;
-
+    //variables for movement x,y 
+    [SerializeField] private int movementSpeed;
+    float movement;
+    bool lookingRight = true;
+    //variables for jumping
+    [SerializeField] private float jumpStrength;
+    [SerializeField] private float delayJump  = 0.5f;
+    private float delayCounter;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        //checking surroundings
-        isGrounded = Physics2D.CircleCast(new Vector2(groundSensor.position.x, groundSensor.position.y), 0.1f, Vector2.zero, 0, LayerMask.GetMask("Middleground"));
-        isStuckOnLeftWall = Physics2D.BoxCast(new Vector2(transform.position.x-0.3f,transform.position.y), new Vector2(0.55f,0.6f), 0.0f, Vector2.right, 0, LayerMask.GetMask("Middleground"));
-        isStuckOnRightWall = Physics2D.BoxCast(new Vector2(transform.position.x+0.4f,transform.position.y), new Vector2(0.65f,0.6f), 0.0f, Vector2.right, 0, LayerMask.GetMask("Middleground"));
-        //isOnStairLeft;
-        //isOnStairRight;
-        // movement on x axis
-        float movement = Input.GetAxis("Horizontal");
-
-        Debug.Log(isStuckOnLeftWall + " " + isStuckOnRightWall);
-        // if stuck on wall dont try to move in its direction
-        if((!isStuckOnLeftWall || movement >= 0) && (!isStuckOnRightWall || movement <= 0))
-        {
-            transform.position += new Vector3(movement*movementSpeed* 0.1f , 0, 0);
-        }
-        
-
-
-        // if the player is on the ground, and was on the ground in the last frame and the player pressed jump and waited the delay, then jump
-        if(isGrounded && Input.GetButton("Jump") && wasGrounded == true && delayCounter >= delayJump)
-        {
-            rb.AddForce(Vector2.up * jumpStrength * 100, ForceMode2D.Impulse);
-        }
-        wasGrounded = isGrounded;
-        
-
-
-    }
-
     void Update()
     {
-        //use jump delay
+        CheckSurroundings();
+        //gets input
+        movement = Input.GetAxis("Horizontal");
+
+        //counter to jump delay
         if(!isGrounded)
         {
             delayCounter = 0;
@@ -72,5 +49,88 @@ public class PlayerMovement : MonoBehaviour
             delayCounter += Time.deltaTime;
         }
 
+        //checks if needs to flip direction
+        if(movement > 0 && !lookingRight)
+        {
+            Flip();
+        }
+        if(movement < 0 && lookingRight)
+        {
+            Flip();
+        }
+
+        HandleAnimations();
+
+
     }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // movement on x axis
+        
+
+        // if stuck on wall dont try to move in its direction
+        if((!isStuckOnLeftWall || movement >= 0) && (!isStuckOnRightWall || movement <= 0))
+        {
+            transform.position += new Vector3(movement*movementSpeed* 0.1f , 0, 0);
+        }
+        
+        // if the player is on the ground, and was on the ground in the last frame and the player pressed jump and waited the delay, then jump
+        if(isGrounded && Input.GetButton("Jump") && wasGrounded == true && delayCounter >= delayJump)
+        {
+            rb.AddForce(Vector2.up * jumpStrength * 100, ForceMode2D.Impulse);
+        }
+        wasGrounded = isGrounded;
+
+        Debug.Log(isGrounded + " " + isStuckOnLeftWall);
+    }
+
+    void CheckSurroundings()
+    {
+        //checks if grounded
+        isGrounded = Physics2D.CircleCast(new Vector2(groundSensor.position.x, groundSensor.position.y), 0.1f, Vector2.zero, 0, LayerMask.GetMask("Middleground"));
+        //checks if next to a wall
+        isStuckOnLeftWall = Physics2D.BoxCast(new Vector2(transform.position.x-0.3f,transform.position.y), new Vector2(0.7f,1.2f), 0.0f, Vector2.right, 0, LayerMask.GetMask("Middleground"));
+        isStuckOnRightWall = Physics2D.BoxCast(new Vector2(transform.position.x+0.4f,transform.position.y), new Vector2(0.7f,1.2f), 0.0f, Vector2.right, 0, LayerMask.GetMask("Middleground"));
+    }
+
+    void Flip()
+    {
+        lookingRight = !lookingRight;
+        //takes the local scale and flips x axis
+        Vector3 Scale = transform.localScale;
+        Scale.x *= -1;
+        transform.localScale = Scale;
+    }
+
+    void HandleAnimations()
+    {   
+        //set Speed for running animations
+        ani.SetFloat("Speed", Mathf.Abs(movement));
+        //get check if jumping up or falling
+        if(!isGrounded)
+        {   
+            //if going up then jumping if going down than falling
+            if(rb.velocity.y >= 0)
+            {
+                ani.SetBool("IsJumping", true);
+                ani.SetBool("IsFalling", false);
+            }
+            else if(rb.velocity.y < 0)
+            {
+                ani.SetBool("IsFalling", true);
+                ani.SetBool("IsJumping", false);
+            }
+        }
+        else
+        {
+            //resets jumping animation
+            ani.SetBool("IsFalling", false);
+            ani.SetBool("IsJumping", false);
+        }
+
+
+    }
+
+
 }
