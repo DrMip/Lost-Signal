3a044d1f582252b71c5062f3daadf0e41a95cbc3
DrMip <ofri.naftali@gmail.com>
2023-11-05 18:42:06 +0200

    Revert "Added Support for multiple Shots"
    This reverts commit 8882f671cbde097e4b6d069bb6241c7cf368c475.

--- Lost Signal/Assets/Code/PlayerCode/ShootingPlayer.cs
+++ Lost Signal/Assets/Code/PlayerCode/ShootingPlayer.cs
@@ -1,22 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using Unity.Mathematics;
 using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class ShootingPlayer : MonoBehaviour
 {
-    //shot list
-    List<Shot> shots = new List<Shot>();
     //other components and refrences
     PlayerBehavior pb;
-    Animator animShot;
-    Animator animPlayer;
+    Animator ani;
     //shot prefab
     public GameObject shotPrefab;
     //shot object
-    //private GameObject shot;
+    private GameObject shot;
     //layercast
     private LayerMask environment;
     private LayerMask enemy;
@@ -28,16 +24,16 @@ public class ShootingPlayer : MonoBehaviour
     private float shotTime;
     private float shotSpeed;
     private float shotDecayTime;
-    //bool isShotRight;
+    bool isShotRight;
     bool pressedShoot;
-    //bool hit;
-    //float Xdirection;
+    bool hit;
+    float Xdirection;
     //temp variables
     [SerializeField] float ShotDistance;
     [SerializeField] float XDisfromCenter = 0.1f;
     //counters
     float shotTimeCounter = 0;
-    //float shotDecayTimeCounter = 0;
+    float shotDecayTimeCounter = 0;
 
     //other variables
     float Xscale;
@@ -45,7 +41,6 @@ public class ShootingPlayer : MonoBehaviour
     void Start()
     {
         pb = GetComponent<PlayerBehavior>();
-        animPlayer = GetComponent<Animator>();
 
         shotTime = pb.ShotTime;
         shotSpeed = pb.ShotSpeed;
@@ -65,22 +60,18 @@ public class ShootingPlayer : MonoBehaviour
         if(Input.GetButtonDown("Fire1"))
         {
             pressedShoot = true;
-            animPlayer.SetBool("Shooting",true);
         }
         //if didnt shot right then fix direction of shoot 
-        for(int i = 0; i < shots.Count; i++)
+        if(!isShotRight && shot)
         {
-            Transform trans = shots[i].shotObject.transform;
-            if(!shots[i].isShotRight && shots[i])
-            {
-                trans.localScale = new Vector3(-Xscale,trans.localScale.y,trans.localScale.z);
-            }
-            //when shot hits or finishes and returns if needs to be deleted
-            if(shots[i].shotObject)
-                CheckandEndShot(shots[i]);
-                
+            shot.transform.localScale = new Vector3(-Xscale,shot.transform.localScale.y,shot.transform.localScale.z);
         }
-
+        //when shot hits or finishes
+        if(shot)
+        {
+  
+            CheckandEndShot();
+        }
         
         
 
@@ -97,15 +88,12 @@ public class ShootingPlayer : MonoBehaviour
             Shoot();
             pressedShoot = false;
         }
-        for(int i = 0; i < shots.Count; i++)
-        {
-            if(!shots[i].hit)
-            {
-                //make it go
-                shots[i].shotObject.transform.position += new Vector3(shots[i].Xdirection*shotSpeed* 0.1f , 0, 0);
-            }
-        }
 
+        if(shot && !hit)
+        {
+            //make it go
+            shot.transform.position += new Vector3(Xdirection*shotSpeed* 0.1f , 0, 0);
+        }
     }
 
     void Shoot()
@@ -113,63 +101,60 @@ public class ShootingPlayer : MonoBehaviour
         //create location to fire
         float xLocation = transform.localScale.x * ShotDistance;
         Vector3 shotPosition =new Vector3 (transform.position.x + xLocation, transform.position.y, transform.position.z);
-        //create object
-        Shot shot = ScriptableObject.CreateInstance<Shot>();
-        //create the shot
-        shot.shotObject = Instantiate(shotPrefab , shotPosition, quaternion.identity);
-        shot.shotObject.name = "shot " + (shots.Count + 1);
         //saves direction of shot
         if(xLocation > 0)
-            shot.isShotRight = true;
+            isShotRight = true;
         else
-            shot.isShotRight = false;
+            isShotRight = false;
 
+        //create the shot
+        shot = Instantiate(shotPrefab , shotPosition, quaternion.identity);
         //get scale
-        Xscale = shot.shotObject.transform.localScale.x;
-        shot.Xdirection = (transform.localScale.x > 0) ? 1:(-1);
+        Xscale = shot.transform.localScale.x;
+        Xdirection = (transform.localScale.x > 0) ? 1:(-1);
         //gets component
-        animShot = shot.shotObject.GetComponent<Animator>();
-        //sets counter
-        shot.shotDecayTimeCounter = 0;
-        shots.Add(shot);
+        ani = shot.GetComponent<Animator>();
+    
 
+
+        
+        
     }
-    void CheckandEndShot(Shot shot)
+    void CheckandEndShot()
     {
         //destroy shot if animation ended (animation would be the exploded because of hit)
-        if(animShot.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.95f && !animShot.IsInTransition(0) && shot.hit)
+        if(ani.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !ani.IsInTransition(0) && hit)
         {
-            shot.hit = false;
-            shot.shotDecayTimeCounter = 0;
-            Destroy(shot.shotObject);
-            shots.Remove(shot);
+            hit = false;
+            shotDecayTimeCounter = 0;
+            Destroy(shot);
             //Debug.Log(hit + " 1");
         }
         //if timer hasnt finished
-        else if(shot.shotDecayTimeCounter < shotDecayTime && !shot.hit)
+        else if(shotDecayTimeCounter < shotDecayTime && !hit)
         {
             //Debug.Log(hit + " 2");
             //continue running timer
-            shot.shotDecayTimeCounter += Time.deltaTime;
+            shotDecayTimeCounter += Time.deltaTime;
             //check for enemies
-            if(ShotSearchFor(enemy,shot))
+            if(ShotSearchFor(enemy))
             {
-                shot.hit = true;
-                animShot.Play(ExplodeAnimEnemy);
+                hit = true;
+                ani.Play(ExplodeAnimEnemy);
                 //Debug.Log(hit + " 3");
             }
-            else if(ShotSearchFor(environment,shot))
+            else if(ShotSearchFor(environment))
             {
-                shot.hit = true;
-                animShot.Play(ExplodeAnimWall);
+                hit = true;
+                ani.Play(ExplodeAnimWall);
                 //Debug.Log(hit + " 4");
             }
         }
-        else if(!shot.hit)
+        else if(!hit)
         {
-            shot.shotDecayTimeCounter = 0;
-            shot.hit = true;
-            animShot.Play(ExplodeAnimTimesUp);
+            shotDecayTimeCounter = 0;
+            hit = true;
+            ani.Play(ExplodeAnimTimesUp);
             //Debug.Log(hit + " 5");
 
         }
@@ -178,12 +163,12 @@ public class ShootingPlayer : MonoBehaviour
         //get what shot collided with
     }
 
-    bool ShotSearchFor(LayerMask layer, Shot shot)
+    bool ShotSearchFor(LayerMask layer)
     {
-        float newxdis = (shot.Xdirection > 0) ? XDisfromCenter:(-XDisfromCenter);
+        float newxdis = (Xdirection > 0) ? XDisfromCenter:(-XDisfromCenter);
     
         //Debug.DrawLine(new Vector2(shot.transform.position.x + newxdis, shot.transform.position.y), new Vector2(shot.transform.position.x + newxdis, shot.transform.position.y + 0.1f), Color.green);
-        return Physics2D.CircleCast(new Vector2(shot.shotObject.transform.position.x + newxdis, shot.shotObject.transform.position.y), 0.1f, Vector2.right, 0f, layer);
+        return Physics2D.CircleCast(new Vector2(shot.transform.position.x + newxdis, shot.transform.position.y), 0.1f, Vector2.right, 0f, layer);
     }
 
 
